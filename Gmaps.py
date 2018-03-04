c9ed96022e23cdd6e572dba1434ef160412ea4f0
archnomed <archnomed@gmail.com>
2018-03-05 02:48:24 +0530

    third commit

--- Gmaps.py
+++ Gmaps.py
@@ -1,6 +1,70 @@
 import urllib.request
 import json
 import sqlite3
+import poly_manip
+import webbrowser
+from mako.template import Template
+
+def plt_route(ls_coordinates):
+    
+    home = 'new google.maps.LatLng'+str(ls_coordinates[0][::-1])+';'
+    pts = ""
+
+    for i in ls_coordinates:
+        pts += 'new google.maps.LatLng'+str(i[::-1])+','
+    
+    pts = pts[:-1]
+    
+    template = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Polyline Plot</title>
+
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+    html { height: 100% }
+    body { height: 100%; margin: 0; padding: 0 }
+    #map_canvas { height: 100% }
+    </style>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js"></script>
+
+    <script>
+        function initialize() {
+            var homeLatlng = ${home}
+            var myOptions = {
+                zoom: 15,
+                center: homeLatlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            // create an array of coordinates
+            var arrCoords = [
+                ${pts}
+            ];
+
+            // draw the route on the map            
+            var route = new google.maps.Polyline({
+                path: arrCoords,
+                strokeColor: "#00FF00",
+                strokeOpacity: 1.0,
+                strokeWeight: 4,
+                geodesic: false,
+                map: map
+            }); 
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+    </head>
+    <body>
+    <div id="map_canvas"></div>
+    </body>
+    </html>
+    """
+    return Template(template).render(home=home,pts=pts)
+
 
 endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
 api_key = 'AIzaSyAyO4O0nLamNnnSVcpUbK6t87otvfWVYz0'
@@ -31,6 +95,15 @@ for i in cur.fetchall():
     print('\n')
 
 conn.close()
+
+print('\n')
+ls_coordinates = poly_manip.decode(polyline) 
+result_html = plt_route(ls_coordinates)
+
+fp = open('map.html','w')
+fp.write(result_html)
+fp.close()
+webbrowser.open_new_tab('map.html')
 
 
 
